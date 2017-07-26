7fb07d97264aee21eb25f49b31bc287cb6947a83
Jay MOULIN <jaymoulin@gmail.com>
2017-07-26 21:55:10 +0200

    Convert to h264

--- convert.sh
+++ convert.sh
@@ -2,6 +2,6 @@
 
 for f in **/*.avi; do
     echo "Converting $f"
-    avconv -i "$f" -c copy "${f%.avi}.mp4" && rm "$f"
+    avconv -i "$f" -strict experimental -c:v libx264 "${f%.avi}.mp4" && rm "$f"
 done 
 echo "Done"
