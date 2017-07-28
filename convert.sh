ae4445a9da13861f3bc32f6175892a3d692c718d
Jay MOULIN <jaymoulin@gmail.com>
2017-07-28 04:21:31 +0200

    Allow sigterm to stop conversion

--- convert.sh
+++ convert.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+trap 'exit 2' TERM INT
+
 for f in **/*.avi; do
+    printf '\033[1;34;40m'
     echo "Converting $f"
+    printf '\033[0m'    
     avconv -i "$f" -strict experimental -c:v libx264 "${f%.avi}.mp4" && rm "$f"
 done 
-echo "Done"
