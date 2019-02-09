5fc9d11dbc3087b911823e06ff1d6c4f7d490582
Jay MOULIN <jaymoulin@gmail.com>
2019-02-09 19:58:54 +0100

    Add webm filetypes

--- convert.sh
+++ convert.sh
@@ -10,7 +10,7 @@ for f in **/*.{avi,ogm,wmv,AVI,OGM,WMV,flv,FLV}; do
     printf '\033[0m'
     avconv -i "$f" -strict experimental -c:v libx264 "${f:0:-4}.mp4" && rm "$f"
 done
-for f in **/*.{divx,DIVX}; do
+for f in **/*.{divx,DIVX,webm,WEBM}; do
     printf '\033[1;34;40m'
     echo "Converting $f"
     printf '\033[0m'
